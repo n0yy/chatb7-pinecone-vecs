2a931519635038cdfe226dcd24552f5c4c9e6e45
Danang H Fadillah <46821909+n0yy@users.noreply.github.com>
2025-03-20 12:00:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -77,8 +77,8 @@ if submit:
         status_text = st.empty()
 
         # Tampilkan informasi ukuran chunk
-        chunk_size = 1000
-        chunk_overlap = 200
+        chunk_size = 1500
+        chunk_overlap = 400
         st.info(
             f"Menggunakan chunk_size={chunk_size} dan chunk_overlap={chunk_overlap} untuk menghindari masalah batas token"
         )
